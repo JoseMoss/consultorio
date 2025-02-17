9f43e777929fdf614e0bc503782ee8356e7a516f
JoseMoss <lasc-mosqueda@hotmail.com>
2025-02-17 13:52:17 -0600

    Resolviendo conflictos

--- obj/Debug/net8.0/ConsultorioOdontologico.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsultorioOdontologico.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsultorioOdontologico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e47be5499b2eb52bdd991f2684931f9a05f1bdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsultorioOdontologico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsultorioOdontologico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
